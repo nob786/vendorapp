c6830a67da067cf49598cd39cc9bf219ac675fc0
Hamza-1997 <hamzaakif97@gmail.com>
2023-08-13 16:50:03 +0500

    unsaved changes fix

--- client/src/utilities/hooks/UnsavedChanged.jsx
+++ client/src/utilities/hooks/UnsavedChanged.jsx
@@ -5,7 +5,11 @@ const UnsavedChangesPrompt = ({ hasUnsavedChanges }) => {
   const [unsavedChanges, setUnsavedChanges] = useState(false);
 
   const handleBeforeUnload = (event) => {
-    if (unsavedChanges) {
+    if (
+      unsavedChanges &&
+      (window.location.pathname.includes("/post-ad") ||
+        window.location.pathname.includes("/edit-ad/"))
+    ) {
       event.preventDefault();
       event.returnValue =
         "You have unsaved changes. Are you sure you want to leave?";
